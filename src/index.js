a2fe2ab93f9181a36e27f5981b621902e74b1a57
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2023-11-29 17:41:47 +0530

    fix: csv files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <>
+  <div style={{ display: "flex", justifyContent: "center", margin: "auto" }}>
     <App />
-  </>
+  </div>
 )
