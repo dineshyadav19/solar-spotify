edf3c98f94290bc3c6e7c1454fab65770ca79356
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2023-11-30 09:24:12 +0530

    fix: app break issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { useState } from "react"
-import { SolarSpotify } from "./components/SolarSpotify"
-import { processCSV } from "./utils/index.ts"
+import React, { useState } from "react"
+import { convertHtmlToImage, processCSV } from "./utils/index.ts"
+import CoverImage from "./CoverImage.tsx"
 
 function CsvReader() {
   const [csvArray, setCsvArray] = useState([])
@@ -18,19 +18,76 @@ function CsvReader() {
     reader.readAsText(file)
   }
 
+  const renderImageHtml = props => `
+    <div class="main--container" id="solar-${props.site_id}">
+      <div style="width: 100vw; height: 100vh;"></div>
+      <div class="container">
+        <div class="main">
+          <h1 class="main--heading">
+            All the care and commitment to your solar plant added up to
+          </h1>
+
+          <h2 class="main--units">${Math.round(props.gen_units)}</h2>
+
+          <p class="main--content">
+            In 2022, your solar plant generated ${Math.round(
+              props.gen_units
+            )} units of electricity that is approximately ~ ₹
+            ${(props.gen_units * 7).toFixed(1)}* and ${Math.round(
+    props.offset_unit
+  )} Kg of Co2 Offset
+          </p>
+        </div>
+        <footer class="footer">
+          <span class="footer--title">#mysolarhome2022</span>
+        </footer>
+      </div>
+    </div>`
+
+  const generateImages = async () => {
+    for (let i = 0; i < csvArray.length; i++) {
+      const currentRow = csvArray[i]
+      const imageHtml = renderImageHtml(currentRow)
+
+      const imageElement = document.createElement("div")
+      imageElement.innerHTML = imageHtml
+
+      document.body.appendChild(imageElement)
+
+      await new Promise(resolve => setTimeout(resolve, 500))
+
+      convertHtmlToImage(currentRow)
+
+      document.body.removeChild(imageElement)
+    }
+  }
+
   return (
     <>
-      <input
-        type="file"
-        accept=".csv"
-        onChange={e => {
-          submit(e.target.files[0])
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          rowGap: "1rem",
+          marginTop: "4rem",
         }}
-      />
-      <br />
-      {csvArray.map(item =>
-        item.site_id ? <SolarSpotify {...item} key={item.site_id} /> : null
-      )}
+      >
+        <input
+          type="file"
+          accept=".csv"
+          onChange={e => {
+            submit(e.target.files[0])
+          }}
+          style={{ width: "100px" }}
+        />
+
+        <input
+          type="button"
+          value="Convert Html to Image"
+          onClick={generateImages}
+        />
+      </div>
     </>
   )
 }
